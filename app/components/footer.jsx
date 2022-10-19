5ccc22c961b24020c78be338afaeaa98c032f742
Jorge Solano Cordero <alexis12115@gmail.com>
2022-10-19 09:28:30 -0600

    Se eliminó el componente Navegacion

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       {/* ----------------------Contenido Footer---------------------- */}
       <div className="container mx-auto flex flex-col items-center md:flex-row md:justify-between">
         {/* ----------------------Navegacion---------------------- */}
-        <nav className="text-white uppercase font-bold text-3xl flex flex-col items-center gap-8 mt-16 md:mt-0 md:flex-row">
+        <nav className="text-white uppercase font-bold text-3xl flex flex-col items-center gap-8 mt-16 md:mt-0 md:flex-row ml-10">
           {/* -----------Index----------- */}
           <Link
             className={
@@ -56,7 +56,7 @@ const Footer = () => {
             Blog
           </Link>
         </nav>
-        <p className="text-white mt-12 md:mt-0 text-center">
+        <p className="text-white mt-12 md:mt-0 text-center mr-10">
           Todos los derechos reservados {new Date().getFullYear()}
         </p>
       </div>
--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,20 +1,68 @@
-import { Link } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
 import logo from "../../public/img/logo.svg";
-import Navegacion from "./navegacion";
 
 const Header = () => {
+  const location = useLocation();
   return (
     /* ----------------------Header---------------------- */
     <header className="header bg-cover bg-no-repeat py-24 bg-center">
       {/* ----------------------Contenido-Header---------------------- */}
       <div className="flex flex-col items-center md:flex-row md:justify-between container mx-auto">
         {/* ----------------------Logo---------------------- */}
-        <Link to="/">
+        <Link className="ml-10" to="/">
           <img className="w-full" src={logo} alt="Imagen logo" />
         </Link>
 
         {/* ----------------------Navegacion---------------------- */}
-        <Navegacion />
+        <nav className="text-white uppercase font-black text-3xl flex gap-8 mt-16 md:mt-0 mx-10 md:ml-0">
+          {/* -----------Index----------- */}
+          <Link
+            className={
+              location.pathname === "/"
+                ? "text-amber-500 p-1.5"
+                : "hover:text-amber-500 p-1.5 transition duration-500"
+            }
+            to="/"
+          >
+            Inicio
+          </Link>
+
+          {/* -----------Nosotros----------- */}
+          <Link
+            className={
+              location.pathname === "/nosotros"
+                ? "text-amber-500 p-1.5"
+                : "hover:text-amber-500 p-1.5 transition duration-500"
+            }
+            to="/nosotros"
+          >
+            Nosotros
+          </Link>
+
+          {/* -----------Tienda----------- */}
+          <Link
+            className={
+              location.pathname === "/tienda"
+                ? "text-amber-500 p-1.5"
+                : "hover:text-amber-500 p-1.5 transition duration-500"
+            }
+            to="/tienda"
+          >
+            Tienda
+          </Link>
+
+          {/* -----------Blog----------- */}
+          <Link
+            className={
+              location.pathname === "/blog"
+                ? "text-amber-500 p-1.5"
+                : "hover:text-amber-500 p-1.5 transition duration-500"
+            }
+            to="/blog"
+          >
+            Blog
+          </Link>
+        </nav>
       </div>
     </header>
   );
